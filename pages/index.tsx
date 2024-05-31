d54423b09f185f3d5847b334f4460523f52c0d7d
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2024-05-31 16:09:00 +0000

    646

--- pages/index.tsx
+++ pages/index.tsx
@@ -91,8 +91,9 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         </div>
       </main>
       <footer className="p-6 text-center text-white/80 sm:p-12">
-       <p>Сайт создан с помощью Next.js, Vercel и Cloudinary.</p>
-       <p>2024 © 10А Столинской государственной гимназии.</p>
+        <p>Сайт создан с помощью Next.js, Vercel и Cloudinary.</p>
+        <p>2024 © 10А Столинской государственной гимназии.</p>
+        <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><span property="dct:title">Фотографии</span> лицензируется под лицензией <a href="https://creativecommons.org/licenses/by-nd/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;" >Creative Commons Attribution-NoDerivatives 4.0 International<img style="height:22px!important; margin-left:3px; vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""></img><img style="height:22px!important; margin-left:3px; vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""></img><img style="height:22px!important; margin-left:3px; vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nd.svg?ref=chooser-v1" alt=""></img></a></p>
       </footer>
     </>
   )
